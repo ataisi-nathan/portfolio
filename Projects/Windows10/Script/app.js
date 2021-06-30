d1d5d112819cf0072e60ce90572c3e64a7f22750
Ataisi Nathan <ataisinathan@gmail.com>
2021-06-30 11:21:58 +0100

    update

--- Projects/Windows10/Script/app.js
+++ Projects/Windows10/Script/app.js
@@ -1,3 +1,23 @@
+// To tell the user to enter fullscreen
+
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+
+var elem = document.body; // Make the body go full screen.
+requestFullScreen(elem);
+
+
 //To display time and date
 function updateTime(){
     var currentTime = new Date()
